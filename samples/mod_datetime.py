942130b380b929f6628a3fe0d38ab5acf1eeb66b
webeautiful <xfs@zyn.local>
2016-04-13 23:34:11 +0800

    mod time

--- samples/mod_datetime.py
+++ samples/mod_datetime.py
@@ -1,8 +1,23 @@
 # -*- coding: utf-8 -*-
 
+# 内建模块
 import datetime
-print '时间处理'
+import time
+print '--时间处理--'
 stime = datetime.datetime.now()
 [x*x for x in range(1, 1000)]
 etime = datetime.datetime.now()
+# 获取时间的毫秒数
 print (etime - stime).microseconds
+
+# 获取时间戳
+timestamp = int(time.time())
+print timestamp
+
+# 将时间戳转换成日期
+timeArr = time.localtime(timestamp)
+print time.strftime('%Y-%m-%d %H:%M:%S', timeArr)
+
+# 将带毫米的日期格式化为常用日期
+withMs = datetime.datetime.now()
+print withMs.strftime('%Y-%m-%d %H:%M:%S')
